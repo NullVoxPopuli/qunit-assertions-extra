2908777a297f8944905ceb5eeec2315a7b5c89b1
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-30 19:49:01 -0400

    fix smoke test script

--- smoke-tests/run-tests.sh
+++ smoke-tests/run-tests.sh
@@ -8,7 +8,7 @@ yarn build
 yarn link
 
 (
-  cd tests/fixtures/ember-3.14/test-app \
+  cd smoke-tests/fixtures/ember-3.14/test-app \
   && yarn \
   && yarn link qunit-assertions-extra \
   && yarn ember test
