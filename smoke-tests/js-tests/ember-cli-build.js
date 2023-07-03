08299bd084fd344fab8df2ab2ca2f25ecef242c2
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-03 15:23:50 -0400

    Improve / test compatibility

---
+++ smoke-tests/js-tests/ember-cli-build.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function (defaults) {
+  const app = new EmberApp(defaults, {
+    // Add options here
+    name: 'js-tests',
+  });
+
+  const { Webpack } = require('@embroider/webpack');
+
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    extraPublicTrees: [],
+    staticAddonTrees: true,
+    staticAddonTestSupportTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+    splitControllers: true,
+    splitRouteClasses: true,
+    implicitModulesStrategy: 'packageNames',
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
+};
